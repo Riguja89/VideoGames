60894f29a17091600c7d2489f8981e1e6b3029c2
Riguja89 <jaimegutierrios@gmail.com>
2022-09-12 22:12:44 -0500

    casi listo el parte get del backend

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
@@ -30,9 +31,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame,Genre } = sequelize.models;
+const { Videogame,Genre,Platform } = sequelize.models;
 Videogame.belongsToMany(Genre,{through: "VideogameGenre"})
 Genre.belongsToMany(Videogame,{through: "VideogameGenre"})
+
+Videogame.belongsToMany(Platform,{through: "VideogamePlatform"})
+Platform.belongsToMany(Videogame,{through: "VideogamePlatform"})
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,16 +1,63 @@
+const { json } = require('body-parser');
 const { Router } = require('express');
-
+const {OP, Videogame}=require('../db.js');
+const axios=require ('axios');
 const router = Router();
-router.get('/',(req, res, next)=>{
-    res.send('Soy get/videogame')
+const {API_KEY} = process.env;
+router.get('/',async (req, res, next)=>{
+    try {
+        const videogamesDb= await Videogame.findAll({});
+        const videogamesApi=[];
+        videogamesApi[0]=await axios.get('https://api.rawg.io/api/games?key='+API_KEY);
+        for(let i=2;i<=5;i++){
+            videogamesApi.push(await axios.get('https://api.rawg.io/api/games?key='+API_KEY+'&page='+i));
+        }
+        
+        Promise.all(videogamesApi).then(resu=>{
+           var VideoGameFiltered=[]
+            videogamesApi.forEach(element=>{
+                VideoGameFiltered=[...VideoGameFiltered,...element.data.results.map( element2=>{               
+                     return {
+                    id: element2.id,
+                    name: element2.name,
+                    image:element2.background_image,
+                    description:"",
+                    released: element2.released,
+                    rating: element2.rating,
+                    platforms: element2.platforms
+                }
+            })
+
+            ]});
+            console.log( VideoGameFiltered);
+            res.json(VideoGameFiltered)
+        });
+    
+        
+             
+    } catch (error) {
+        next(error);
+    }
+ 
 })
 
 router.put('/',(req, res, next)=>{
-    res.send('Soy out/videogame')
+    res.send('Soy put/videogame')
 })
 
-router.post('/',(req, res, next)=>{
-    res.send('Soy post/videogame')
+router.post('/',async(req, res, next)=>{
+    const{name, description,released, rating}=req.body;
+    try {
+        const newVideoGame= await Videogame.create(
+            {
+                name, description,released, rating
+            }
+        );
+        res.status(201).json(newVideoGame);
+    } catch (error) {
+        next(error);
+    }
+   
 })
 
 router.delete('/',(req, res, next)=>{
