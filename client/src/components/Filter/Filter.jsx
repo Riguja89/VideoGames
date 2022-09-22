beb3eceef3ff7d579a1da47bf76a7a558a1497ee
Riguja89 <jaimegutierrios@gmail.com>
2022-09-22 10:42:35 -0500

    finalizado lo de Filtros ahora a modificar la parte de ordenado en general

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -3,68 +3,72 @@ import { useDispatch } from 'react-redux';
 import { getGenres,setGamestoShow } from '../../redux/actions';
 import { useEffect } from 'react';
 import { connect } from 'react-redux';
-
+import { useState } from 'react';
 
 const FilterCont = (props) => {
 
     const dispatch=useDispatch();
-    
+    const [filteredbyGenre, setfilteredbyGenre]=useState([]);
+    const [filteredbyDB, setfilteredbyDB]=useState([]);
+
 useEffect(()=>{
     if(props.genres.length===0) dispatch(getGenres()); //caga generos desde la db si no esta lleno 
-})
-
-useEffect(()=>{
-   var chageselect=document.getElementById("genreselect");
-    chageselect.value="All";
+    setfilteredbyGenre(props.videogames);
+    setfilteredbyDB(props.videogames);
 },[props.videogames])
 
-function handleGenre(e){
+function handleFilterGenre(e){
     let filtered=[]
-    if(e.target.id==="genreselect"){
     if (e.target.value!=="All" ){
-  props.videogames.forEach((game)=>{
-   if(game.genres.filter((gen)=>(gen.id===parseInt(e.target.value))).length>0){
-    filtered=[...filtered,game]
-   }
 
-  })
-    //console.log(filtered);
-    dispatch(setGamestoShow(filtered));}else{
-    dispatch(setGamestoShow(props.videogames));
+        filteredbyDB.forEach((game)=>{
+            if(game.genres.filter((gen)=>(gen.id===parseInt(e.target.value))).length>0){
+               filtered=[...filtered,game]
+            }
+                })
+        dispatch(setGamestoShow(filtered));
+        filtered=[];
+        props.videogames.forEach((game)=>{
+            if(game.genres.filter((gen)=>(gen.id===parseInt(e.target.value))).length>0){
+             filtered=[...filtered,game]
+            }
+                })
+        setfilteredbyGenre(filtered);
+    }else{
+        setfilteredbyGenre(props.videogames)
+        dispatch(setGamestoShow(filteredbyDB));
     }
-    document.getElementById("whereselect").value="All"
-}else{
-    if(e.target.value==="db"){
-        filtered= props.gamestoShow.filter(game=>(game.id.length>9))
-        dispatch(setGamestoShow(filtered))
-     }else{
-
-        if (document.getElementById("genreselect").value!=="All" ){
-            props.videogames.forEach((game)=>{
-             if(game.genres.filter((gen)=>(gen.id==document.getElementById("genreselect").value)).length>0){
-              filtered=[...filtered,game]
-             }})
-              dispatch(setGamestoShow(filtered));}else{
-                dispatch(setGamestoShow(props.videogames));
-              }
-
-        
-     }
-     
 }
 
+function handleFilterDB(e){
+    let filtered=[]
+
+    switch(e.target.value){
+        case "All":
+                filtered=filteredbyGenre;
+                setfilteredbyDB(props.videogames);
+            break;
+        case "db":
+                filtered= filteredbyGenre.filter(game=>(game.id.length>9));
+                setfilteredbyDB(props.videogames.filter(game=>(game.id.length>9)));
+            break;
+        case "rawg":
+                filtered= filteredbyGenre.filter(game=>(typeof(game.id)!=="string"));
+                setfilteredbyDB(props.videogames.filter(game=>typeof(game.id)!=="string"));
+             break;    
+
+        default:
+            break;
+    }
+    dispatch(setGamestoShow(filtered));
+    
 }
 
-// function handlewere(e){
-//     let filterdb=[]
-//     if(e.target.value==="db"){
-//        filterdb= props.gamestoshow.filter(game=>(game.id.length>9))
-//     }
-// }
+
     return (
         <div>
                 <label>Genres: </label>
-                <select id="genreselect" name="genres" onChangeCapture={handleGenre}> {props.genres!==undefined ?props.genres.map(genre=>{
+                <select id="genreselect" name="genres" onChangeCapture={handleFilterGenre}> {props.genres!==undefined ?props.genres.map(genre=>{
                    
                    return(
                         <option key={genre.id} value={genre.id}>{genre.name}</option>
@@ -73,9 +77,10 @@ function handleGenre(e){
                 <option value="All" selected>All</option>
                 </select>
                 <label>Where: </label>
-                <select id="whereselect" name="where" onChangeCapture={handleGenre}> 
+                <select id="whereselect" name="where" onChangeCapture={handleFilterDB }> 
                 <option value="All" selected>All</option>
-                <option value="db" selected>My DB</option>
+                <option value="db">My DB</option>
+                <option value="rawg">RAWG</option>
                 </select>      
         </div>
     );
