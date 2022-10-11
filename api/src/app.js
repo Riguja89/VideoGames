0940f3bdde1049178385c1c435d65e65347cbbe8
Riguja89 <jaimegutierrios@gmail.com>
2022-10-11 13:59:05 -0500

    Se actualizó politica cors en app.js

--- api/src/app.js
+++ api/src/app.js
@@ -15,8 +15,9 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
+ // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // activar para que permita cors local
+ res.header('Access-Control-Allow-Origin', 'https://videogame-clien-api.vercel.app');  // CORS EN EL DEPLY
+ res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
